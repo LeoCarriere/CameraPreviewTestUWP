8f226a549de52290b346f0ba9fb497ff8852d8c7
CARRIERE Léo <developpement2@helpilot.fr>
2022-06-13 10:52:21 +0200

    Add project files.

---
+++ App11-UWP/MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+using Microsoft.Toolkit.Uwp.Helpers;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace App11_UWP
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+       private async void myButton_Click( object sender , RoutedEventArgs e ) {
+            myButton.Content = "Clicked";
+            await CameraPreviewControl.StartAsync();
+            CameraPreviewControl.CameraHelper.FrameArrived += CameraPreviewControl_FrameArrived;
+
+        }
+
+        private void CameraPreviewControl_FrameArrived(object sender, FrameEventArgs e)
+        {
+             var videoFrame = e.VideoFrame;
+             var softwareBitmap = videoFrame.SoftwareBitmap;
+        }
+    }
+}
